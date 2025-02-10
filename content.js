a587199e5ee9c50f4f7e26c0fd1f17b8bfdc6245
André Ribeiro <andremezzaliraribeiro@gmail.com>
2025-02-10 09:05:03 -0300

    update: remove log

--- content.js
+++ content.js
@@ -2,7 +2,6 @@
     function stayFullscreen() {
       const player = document.querySelector("video");
       if (!document.fullscreenElement && player && !player.paused) {
-        console.log("Re-entering fullscreen...");
         document.documentElement.requestFullscreen();
       }
     }
